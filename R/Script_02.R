a9e1eb397cc824f380c338e2ba2cb976346a5b2d
Redcart <simon.corde@hotmail.fr>
2019-01-28 17:54:12 +0100

    Split en fichier des fonctions

--- R/Script_02.R
+++ R/Script_02.R
@@ -33,141 +33,141 @@ library(dplyr)
 
 kmeans_procedure <- function(data, columns, threshold_min, threshold_max, verbose = FALSE, seed = 42)
 {
-  
+
   set.seed(seed)
-  
+
   i <- 1
   b <- 1
   k <- 2
   c <- 1
-  
+
   n_observation <- 1:dim(data)[1]
   cluster <- NA
-  
+
   liste_clusterise <- data.frame(n_observation, cluster)
   table(liste_clusterise$cluster)
-  
+
   test <- FALSE
   ## While clusters are not all filled
-  
+
   while (test == FALSE)
     {
-    
+
       if (verbose == TRUE)
        {
-      
+
         print(paste("boucle : " , b, " nb de clusters : ", k, " N° cluster : ", c))
-    
+
        }
-    
-    
-      liste_temp <- liste_clusterise %>% 
-        filter(is.na(cluster)) %>% 
-        rename(cluster_bis = cluster) %>% 
+
+
+      liste_temp <- liste_clusterise %>%
+        filter(is.na(cluster)) %>%
+        rename(cluster_bis = cluster) %>%
         select(n_observation, cluster_bis)
-      
-    
+
+
       ## We perform clustering on observations that do not have one
       set.seed(seed)
-      
-      assign(paste("model_kmean_", b, ".", k, sep = ""), 
+
+      assign(paste("model_kmean_", b, ".", k, sep = ""),
              kmeans(data[liste_temp$n_observation, columns], centers = k, iter.max = 50, nstart = 5, algorithm = "Lloyd"))
-      
-      assign(paste("volumetrie_cluster_", b, ".", k, sep = ""), 
+
+      assign(paste("volumetrie_cluster_", b, ".", k, sep = ""),
              table(eval(as.name(paste("model_kmean_", b, ".", k, sep = "")))$cluster))
-      
+
       eval(as.name(paste("volumetrie_cluster_", b, ".", k, sep = "")))
-      
+
       # If at least one cluster has size between threshold_min and threshold_max
-      if (any(between(eval(as.name(paste("volumetrie_cluster_", b, ".", k, sep = ""))), threshold_min, threshold_max))) 
+      if (any(between(eval(as.name(paste("volumetrie_cluster_", b, ".", k, sep = ""))), threshold_min, threshold_max)))
        {
-        
+
         # We fill the cluster(s) in that case
         for (i in 1:k)
           {
-          
+
           # We also fill clusters whose size would fall below the minimum treshold
-            if (eval(as.name(paste("volumetrie_cluster_", b, ".", k, sep = "")))[i] <= threshold_max) 
+            if (eval(as.name(paste("volumetrie_cluster_", b, ".", k, sep = "")))[i] <= threshold_max)
              {
-            
+
                liste_temp$cluster_bis[eval(as.name(paste("model_kmean_", b, ".", k, sep = "")))$cluster==i] <- c
-            
-               liste_clusterise <- liste_clusterise %>% 
-                 left_join(liste_temp, by = "n_observation") %>% 
-                 mutate(cluster = ifelse(is.na(cluster), cluster_bis, cluster)) %>% 
+
+               liste_clusterise <- liste_clusterise %>%
+                 left_join(liste_temp, by = "n_observation") %>%
+                 mutate(cluster = ifelse(is.na(cluster), cluster_bis, cluster)) %>%
                  select(-cluster_bis)
-                
+
                  c <- c + 1
-                
+
              }
-          
+
           }
-        
+
           b <- b + 1
           k <- 2
-        
-       }          
-      
+
+       }
+
       # If every size cluster is above the maximum threshold we keep increasing the number of clusters
       else if (all(eval(as.name(paste("volumetrie_cluster_", b, ".", k, sep = ""))) > threshold_max))
-        {  
-        
-          k <- k + 1 
-        
-        } 
-      
-      # If one cluster has size below the minimum threshold and the other above the maximum threshold 
+        {
+
+          k <- k + 1
+
+        }
+
+      # If one cluster has size below the minimum threshold and the other above the maximum threshold
       else if (any(eval(as.name(paste("volumetrie_cluster_", b, ".", k, sep = ""))) < threshold_min))
         {
-        
+
         # We get back to the previous kmeans and fill the clusters
         for (i in 1:(k-1))
           {
             liste_temp$cluster_bis[eval(as.name(paste("model_kmean_", b, ".", k-1, sep = "")))$cluster==i] <- c
-            
-            liste_clusterise <- liste_clusterise %>% 
-              left_join(liste_temp, by = "n_observation") %>% 
-              mutate(cluster = ifelse(is.na(cluster), cluster_bis, cluster)) %>% 
+
+            liste_clusterise <- liste_clusterise %>%
+              left_join(liste_temp, by = "n_observation") %>%
+              mutate(cluster = ifelse(is.na(cluster), cluster_bis, cluster)) %>%
               select(-cluster_bis)
-            
+
             c <- c + 1
-          
+
           }
-        
+
        }
-      
+
       else
         {# Test to be sure to not forget a case
-        
+
         print(" Be careful another case exists !")
-        
+
         }
       # Are all observations clusterized ?
-      test <- sum(is.na(liste_temp$cluster)) == 0     
-      
+      test <- sum(is.na(liste_temp$cluster)) == 0
+
       if (verbose  == TRUE)
        {
-        
+
          print(table(liste_clusterise$cluster))
-        
+
        }
-      
+
     }
-  
+
     results <- list()
     results$clusters <- liste_clusterise$cluster
     results$frequencies <- table(liste_clusterise$cluster)
-    
+
     return(results)
-  
+
 }
 
 ############################################
 ###  Performing example of the function  ###
 ############################################
 
-data <- trees
-str(data)
-
-final <- kmeans_procedure(data = data, columns = 1:3, threshold_min = 3, threshold_max = 5)
-final
+# data <- trees
+# str(data)
+#
+# final <- kmeans_procedure(data = data, columns = 1:3, threshold_min = 3, threshold_max = 5)
+# final
---
+++ R/create_calendar_day.R
@@ -0,0 +1,41 @@
+
+rm(list = ls())
+
+### Loading of required R packages
+
+library(ggplot2)
+library(dplyr)
+library(sqldf)
+library(stringr)
+
+##############################################
+###  Function for complete empty calendar  ###
+##############################################
+
+# This function allows to create a complete empty calendar
+# arguments:
+# - data: R data frame
+# - key_variable: variable name that refers to the key variable in the panel (ID, ...)
+# - time_variable: time variable name that permits to sort observation on a time scale
+# - start_year: starting year of the time serie
+# - end_year: ending year of the time serie
+
+create_calendar_day <- function(data, key_variable, time_variable, start_year, end_year){
+  
+  #, scale_time
+  
+  ids <- data %>% 
+    select(key_variable) %>% 
+    distinct()
+  
+  years <- data.frame(start_year:end_year)
+  
+  calendar <- sqldf("SELECT * 
+                    FROM ids
+                    CROSS JOIN years")
+  
+  colnames(calendar) <- c(key_variable, time_variable)
+  
+  return(calendar)
+  
+}
