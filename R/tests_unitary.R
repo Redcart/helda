f3d3859b8a17651e14e358faf72c152ed9d701e3
Simon Corde <simon.corde@hotmail.fr>
2019-11-03 12:31:11 +0100

    add results file for unitary tests and change relative path

---
+++ R/tests_unitary.R
@@ -0,0 +1,14 @@
+library(testthat)
+
+test_that("single number", {
+  
+  data <- iris
+  
+  str(data)
+  
+  result_array <- proc_freq(data$Species)
+  
+  expected_result <- read.csv("results_proc_freq.csv")
+
+  expect_equal(result_array, expected_result)
+})
