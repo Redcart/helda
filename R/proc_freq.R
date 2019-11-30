761365c9191726091f04bf700c137114baf97e17
Simon Corde <simon.corde@hotmail.fr>
2019-11-30 11:29:43 +0100

    tests for a new clean documentation

--- R/proc_freq.R
+++ R/proc_freq.R
@@ -11,29 +11,40 @@
 #'
 #' @param variable vector on which we want to apply the function
 #' @param digits: integer that specifies the number of decimals we want to keep in the rounded figures
+#' @return data frame of dimension [nb of categories x 5].
+#' The five columns diplay the following information:
+#' * the first one shows the different categories of the variable on which we want to perform proc_freq
+#' * then we have frequency, percentage, cumulative frequency and cumulative percentage
+#' @author Simon CORDE
+#' @keywords proc_freq frequency table categorical variable SAS
+#' @references Link to the author's github repository:
+#' https://www.github.com/Redcart
 #' @export
 #' @examples
-#' proc_freq(variable, digits = 4)
+#' data <- iris
+#' str(data)
+#' result_array <- proc_freq(data$Species)
+#' View(result_array)
 #'
 
 
 proc_freq <- function(variable, digits = 4)
 {
-  
+
   categories <- names(sort(table(variable)))
   frequencies <- as.vector(sort(table(variable)))
-  
+
   n <- sum(frequencies)
   percentages <- round(frequencies / n, digits)*100
   cum_frequencies <- cumsum(frequencies)
   cum_percentages <- cumsum(percentages)
-  
-  result <- data.frame("Category" = categories, 
-                       "Frequency" = frequencies, 
-                       "Percentage" = percentages, 
+
+  result <- data.frame("Category" = categories,
+                       "Frequency" = frequencies,
+                       "Percentage" = percentages,
                        "Cumulative.Frequency" = cum_frequencies,
                        "Cumulative.Percentage" = cum_percentages)
-  
+
   return(result)
-  
+
 }
