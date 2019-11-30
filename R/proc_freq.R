937a9b383a03775ff12160e1a0cfd0c0e194fd4f
Simon Corde <simon.corde@hotmail.fr>
2019-11-30 11:44:06 +0100

    tests for a new clean documentation

--- R/proc_freq.R
+++ R/proc_freq.R
@@ -7,6 +7,8 @@
   # variable: vector on which we want to apply the function
   # digits: integer that specifies the number of decimals we want to keep in the rounded figures
 
+#' SAS proc freq in R
+#'
 #' This function permits to reproduce the output of the SAS proc freq
 #'
 #' @param variable vector on which we want to apply the function
@@ -14,12 +16,12 @@
 #' @return data frame of dimension [nb of categories x 5].
 #' @details
 #' The five columns diplay the following information:
-#' *the first one shows the different categories of the variable on which we want to perform proc_freq
-#' *then we have frequency, percentage, cumulative frequency and cumulative percentage
+#'  the first one shows the different categories of the variable on which we want to perform proc_freq
+#'  then we have frequency, percentage, cumulative frequency and cumulative percentage
 #' @author Simon CORDE
 #' @keywords proc_freq frequency table categorical variable SAS
 #' @references Link to the author's github repository:
-#' https://www.github.com/Redcart
+#' \url{https://www.github.com/Redcart}
 #' @export
 #' @examples
 #' data <- iris
