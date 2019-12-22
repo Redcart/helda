3f66daf415c541dfd91482cd756d8a18e63091a7
Simon Corde <simon.corde@hotmail.fr>
2019-12-22 23:40:06 +0100

    modify travis config file

--- tests/testthat/test-proc_freq.R
+++ tests/testthat/test-proc_freq.R
@@ -1,15 +1,15 @@
 library(helda)
+
 context('Proc Freq Function')
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
 
-print(getwd())
-data <- iris
-load(file = "../../results_proc_freq.RData")
-
 test_that("proc freq", {
+  print(getwd())
+  data <- iris
+  load(file = "../../results_proc_freq.RData")
   expect_equal(helda::proc_freq(data$Species), result_array)
 })
 
