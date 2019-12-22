b14f31efc224eaa65ade59275e923b2f81d651e3
Simon Corde <simon.corde@hotmail.fr>
2019-12-22 20:00:46 +0100

    first unit test for proc freq

--- tests/testthat/test-proc_freq.R
+++ tests/testthat/test-proc_freq.R
@@ -1,3 +1,12 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 5)
+  expect_equal(2 * 2, 4)
 })
+
+
+data <- iris
+result_proc_freq <- load(file = "results_proc_freq.RData")
+
+test_that("proc freq", {
+  expect_equal(proc_freq(data$Species), result_proc_freq)
+})
+
