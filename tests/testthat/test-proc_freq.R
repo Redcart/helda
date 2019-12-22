546e1c186e6e8a8b3e3a337455a38a5a962e0d02
Simon Corde <simon.corde@hotmail.fr>
2019-12-22 21:08:58 +0100

    modify travis config file

--- tests/testthat/test-proc_freq.R
+++ tests/testthat/test-proc_freq.R
@@ -5,7 +5,7 @@ test_that("multiplication works", {
 
 print(getwd())
 data <- iris
-result_proc_freq <- load(file = "results_proc_freq.RData")
+result_proc_freq <- load(file = "../../results_proc_freq.RData")
 
 test_that("proc freq", {
   expect_equal(proc_freq(data$Species), result_proc_freq)
