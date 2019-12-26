696e9336c04102162b8d44bb851b2beebc9a930f
Simon Corde <simon.corde@hotmail.fr>
2019-12-26 20:20:37 +0100

    new test

--- R/create_formula.R
+++ R/create_formula.R
@@ -7,10 +7,34 @@
   # data: R data frame
   # position: number of the column in the data frame that we want to predict
 
+#' Create a formula
+#'
+#' This function permits to reproduce the output of the SAS proc freq
+#'
+#' @param variable vector on which we want to apply the function
+#' @param digits integer that specifies the number of decimals we want to keep in the rounded figures
+#' @return data frame of dimension [nb of categories x 5].
+#' @details
+#' The five columns diplay the following information:
+#' \itemize{
+#'  \item the first one shows the different categories of the variable on which we want to perform proc_freq
+#'  \item then we have frequency, percentage, cumulative frequency and cumulative percentage
+#'  }
+#' @author Simon CORDE
+#' @keywords proc_freq frequency table categorical variable SAS
+#' @references Link to the author's github repository:
+#' \url{https://www.github.com/Redcart}
+#' @export create_formula
+#' @examples
+#' data <- iris
+#' str(data)
+#' result_array <- proc_freq(data$Species)
+#'
+
 create_formula <- function(data, position = 1)
 {
-  
+
   formula <- paste(colnames(data)[position], " ~ ", paste(colnames(data[-position]), collapse = " + "), sep = "")
   return(formula)
-  
+
 }
--- tests/testthat/test-create_formula.R
+++ tests/testthat/test-create_formula.R
@@ -1,16 +1,15 @@
 library(helda)
 #
-context('Proc Freq Function')
+context('Create formula function')
 
 # test_that("multiplication works", {
 #   expect_equal(2 * 2, 4)
 # })
 
-test_that("proc freq", {
+test_that("create formula", {
   #print(getwd())
   data <- iris
-  data_expected <- results_proc_freq
+  data_expected <- formula_test
   #print(helda::proc_freq(data$Species))
-  expect_equal(proc_freq(data$Species), data_expected)
+  expect_equal(create_formula(data), data_expected)
 })
-
