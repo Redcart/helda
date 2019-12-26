a37f4a26f9147fff13a1c1e596f20a086c8fa6f3
Simon Corde <simon.corde@hotmail.fr>
2019-12-26 12:52:28 +0100

    new folder data and documentation for it

--- tests/testthat/test-proc_freq.R
+++ tests/testthat/test-proc_freq.R
@@ -7,11 +7,10 @@ context('Proc Freq Function')
 # })
 
 test_that("proc freq", {
-  print(getwd())
+  #print(getwd())
   data <- iris
   load(file = "../../data/results_proc_freq.RData")
-  print(data)
-  print(helda::proc_freq(data$Species))
+  #print(helda::proc_freq(data$Species))
   expect_equal(helda::proc_freq(data$Species), results_proc_freq)
 })
 
