6cfd8ccdfd90b1925484c79335fdd05648de7614
Simon Corde <simon.corde@hotmail.fr>
2020-01-18 15:09:08 +0100

    new unit and integrations tests with more real word data

--- R/start_end_to_fill.R
+++ R/start_end_to_fill.R
@@ -26,21 +26,23 @@
 #' @export start_end_to_fill
 #' @export gap_to_fill
 #' @examples
-#' rep(c("Paris", "Madrid", "Berlin"), each = 10)
-#' jeu_donnees <- data.frame("country" = rep(c("France", "Spain", "Germany"), each = 10),
-#' "capital" = rep(c("Paris", "Madrid", "Berlin"), each = 10),
-#' "year" = 2009:2018,
-#' "gdp" = c(NA, NA, 200, 300, 500, 1000, NA, NA, NA, 500,
-#'  0, NA, NA, NA, NA, NA, NA, 800, 1200, 1500,
-#'  100, 200, 400, 700, 700, 800, 600, 500, NA, NA))
-#'  jeu_donnees <- na.omit(jeu_donnees)# we artificially create some gaps in the time series
-#'  data_1 <- create_calendar_day(data = jeu_donnees, key_variable = "country", time_variable = "year",
-#'  start_year = 2009, end_year = 2018)
-#'  data_2 <- start_end_to_fill(data = jeu_donnees, calendar = data_1, gap_variable = "gdp",
-#'  key_variable = "country", time_variable = "year")
-#'  data_3 <- gap_to_fill(data = data_2, gap_variable = "gdp_corrected_1", key_variable = "country",
-#'  time_variable = "year", digits = 1)
-
+#' library(dplyr)
+#' # We take three countries from 2011 to 2018
+#' fr_sp_ge_pop <- world_countries_pop %>%
+#' filter(country_name %in% c('France', 'Spain', 'Germany')) %>%
+#' filter(year > 2010) %>%
+#' arrange(country_name, year)
+#'
+#' # We artificially create some gaps in time series
+#' fr_sp_ge_pop$population[c(1, 5, 11, 12, 24)] <- NA
+#' fr_sp_ge_pop <- na.omit(fr_sp_ge_pop)
+#'
+#' data_1 <- create_calendar_day(data = fr_sp_ge_pop, key_variable = "country_code",
+#' time_variable = "year", start_year = 2011, end_year = 2018)
+#' data_2 <- start_end_to_fill(data = fr_sp_ge_pop, calendar = data_1, gap_variable = "population",
+#' key_variable = "country_code", time_variable = "year")
+#' data_3 <- gap_to_fill(data = data_2, gap_variable = "population_corrected_1",
+#' key_variable = "country_code", time_variable = "year", digits = 1)
 
 start_end_to_fill <- function(data, calendar, gap_variable, key_variable, time_variable){
 
