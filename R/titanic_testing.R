cbb08527c2d839bddb1ced66fe8a362239e8707d
Simon Corde <simon.corde@hotmail.fr>
2020-02-19 21:47:12 +0100

    updates documentation for data sets

--- R/titanic_testing.R
+++ R/titanic_testing.R
@@ -1,7 +1,20 @@
 #' @title Titanic testing data set
 #'
 #' @source Kaggle Titanic Competition: \url{https://www.kaggle.com/c/titanic/data}
-#' @format Data frame of 418 observations and 10 features
+#' @format Data frame of 418 observations and 11 features:
+#' \describe{
+#' \item{PassengerId}{id of the passenger}
+#' \item{Pclass}{passenger class on the boat}
+#' \item{Name}{name of the passenger}
+#' \item{Sex}{male / female}
+#' \item{Age}{age of the passenger}
+#' \item{SibSp}{number of siblings/spouses aboard}
+#' \item{Parch}{number of parents/children aboard}
+#' \item{Ticket}{ticket no}
+#' \item{Fare}{price of the ticket}
+#' \item{Cabin}{location of the cabin on the boat}
+#' \item{Embarked}{harbor city of boarding}
+#' }
 #' @examples
 #' data(titanic_testing)
 "titanic_testing"
--- R/world_countries_pop.R
+++ R/world_countries_pop.R
@@ -1,5 +1,6 @@
 #' @title World countries population from 1960 to 2018
 #'
+#' @source World Bank website \url{https://data.worldbank.org/indicator/SP.POP.TOTL}
 #' @format R Data frame with 15576 obervations and 4 variables
 #' \describe{
 #' \item{country_name}{name of the country}
