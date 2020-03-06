8c361cb1892c69a5eaf88d153e95b6b3adc5c9a8
Simon Corde <simon.corde@hotmail.fr>
2020-03-06 12:57:10 +0100

    add print for debugging

--- tests/testthat/test-lift_curve.R
+++ tests/testthat/test-lift_curve.R
@@ -5,11 +5,16 @@ context('lift curve function')
 test_that("lift curve for titanic data set", {
   data_training <- titanic_training
   data_validation <- titanic_validation
+  print(str(data_training))
+  print(str(data_validation))
   model_glm <- glm(formula="Survived ~ Pclass + Sex + Age + SibSp + Fare + Embarked",
                    data=data_training,
                    family=binomial(link="logit"))
   predictions <- predict(object=model_glm, newdata=data_validation, type="response")
+  print(str(predictions))
   result <- lift_curve(predictions=predictions, true_labels=data_validation$Survived, positive_label=1)
   load(file = 'plot_lift_curve_test.Rda')
+  print(str(result))
+  print(str(plot_lift_curve_test))
   expect_equal(result, plot_lift_curve_test)
 })
