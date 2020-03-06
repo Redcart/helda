fa3886f21422e3f2af523b5e65b4ba9925671bdf
Simon Corde <simon.corde@hotmail.fr>
2020-03-06 15:01:39 +0100

    replace some unit tests

--- tests/testthat/test-lift_curve.R
+++ tests/testthat/test-lift_curve.R
@@ -3,13 +3,13 @@ library(helda)
 context('lift curve function')
 
 test_that("lift curve for titanic data set", {
-  #data_training <- titanic_training
-  #data_validation <- titanic_validation
-  #print(str(data_training))
-  #print(str(data_validation))
-  #model_glm <- glm(formula="Survived ~ Pclass + Sex + Age + SibSp + Fare + Embarked",
-                   #data=data_training,
-                   #family=binomial(link="logit"))
+  data_training <- titanic_training
+  data_validation <- titanic_validation
+  print(str(data_training))
+  print(str(data_validation))
+  model_glm <- glm(formula="Survived ~ Pclass + Sex + Age + SibSp + Fare + Embarked",
+                   data=data_training,
+                   family=binomial(link="logit"))
   #predictions <- predict(object=model_glm, newdata=data_validation, type="response")
   #print(str(predictions))
   #result <- lift_curve(predictions=predictions, true_labels=data_validation$Survived, positive_label=1)
