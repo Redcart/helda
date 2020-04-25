b2252050562f2895d5ef106ca21a24cae45b6633
Simon Corde <simon.corde@hotmail.fr>
2020-04-25 13:24:49 +0200

    unit tests ofr ggplot2 graphics with R v4.0.0

---
+++ tests/testthat/test-lift_curve.R
@@ -0,0 +1,15 @@
+library(helda)
+
+context('lift curve function')
+
+test_that("lift curve for titanic data set", {
+  data_training <- titanic_training
+  data_validation <- titanic_validation
+  model_glm <- glm(formula="Survived ~ Pclass + Sex + Age + SibSp + Fare + Embarked",
+                   data=data_training,
+                   family=binomial(link="logit"))
+  predictions <- predict(object=model_glm, newdata=data_validation, type="response")
+  result <- lift_curve(predictions=predictions, true_labels=data_validation$Survived, positive_label=1)
+  load(file='lift_curve_test.Rda')
+  expect_equal(result, lift_curve_test)
+})
---
+++ tests/testthat/test-lift_effect.R
@@ -0,0 +1,15 @@
+library(helda)
+
+context('lift effect function')
+
+test_that("lift effect for titanic data set", {
+  data_training <- titanic_training
+  data_validation <- titanic_validation
+  model_glm <- glm(formula="Survived ~ Pclass + Sex + Age + SibSp + Fare + Embarked",
+                   data=data_training,
+                   family=binomial(link="logit"))
+  predictions <- predict(object=model_glm, newdata=data_validation, type="response")
+  result <- lift_effect(predictions=predictions, true_labels=data_validation$Survived, positive_label=1)
+  load(file='lift_effect_test.Rda')
+  expect_equal(result, lift_effect_test)
+})
